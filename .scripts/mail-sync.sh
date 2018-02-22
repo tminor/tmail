ed6948dc56ebb9bf9a6fd8bdc6650f144ac17d49
tminor <thomas.minor@usg.edu>
2018-02-22 11:28:16 -0500

    Change order of command execution
    spam-filter.py removes new flag in all cases; executing it before
    afew causes afew to not work.

--- .scripts/mail-sync.sh
+++ .scripts/mail-sync.sh
@@ -17,19 +17,37 @@ MAIL_NOTIFY="/Users/$(id -un)/.scripts/mail-notify.sh"
 
 if [ $LOGGING_ENABLED = true ]; then
   echo "$(date): PATH is $PATH" >> $LOG_FILE
+  printf "\n" >> $LOG_FILE
 
+  # Sync mail
   echo '### mbsync output and exit status ###' >> $LOG_FILE
   printf "\n" >> $LOG_FILE
   /usr/local/bin/mbsync -a >> $LOG_FILE 2>&1
   echo "$(date): mbsync exited with status $?" >> $LOG_FILE
   printf "\n" >> $LOG_FILE
 
+  # Tag new mail with 'new'
   echo '### notmuch output and exit status ###' >> $LOG_FILE
   printf "\n" >> $LOG_FILE
   /usr/local/bin/notmuch new >> $LOG_FILE 2>&1
   echo "$(date): notmuch exited with status $?" >> $LOG_FILE
   printf "\n" >> $LOG_FILE
 
+  # Tag qualifying 'new' mail with 'spam'
+  echo '### bogofilter script output ###'
+  printf "\n" >> $LOG_FILE
+  /usr/local/bin/python3 /Users/tminor/.scripts/spam-filter.py >> $LOG_FILE 2>&1
+  echo "$(date): spam-filter.py exited with status $?" >> $LOG_FILE
+  printf "\n" >> $LOG_FILE
+
+  # Filter with afew
+  echo '### afew output and exit status ###' >> $LOG_FILE
+  printf "\n" >> $LOG_FILE
+  /Users/tminor/.venv/bin/afew --tag --new -vv >> $LOG_FILE 2>&1
+  echo "$(date): afew exited with status $?" >> $LOG_FILE
+  printf "\n" >> $LOG_FILE
+
+  # Finally, notify of new mail
   echo '### mail-notify.sh output and exit status ###' >> $LOG_FILE
   printf "\n" >> $LOG_FILE
   /bin/bash $MAIL_NOTIFY >> $LOG_FILE 2>&1
@@ -38,11 +56,13 @@ if [ $LOGGING_ENABLED = true ]; then
 else
   /usr/local/bin/mbsync -a
   /usr/local/bin/notmuch new
+  /Users/tminor/.venv/bin/afew --tag --new
+  /usr/local/bin/python3 /Users/tminor/.scripts/spam-filter.py
   /bin/bash $MAIL_NOTIFY
 fi
 
 # TODO
 # -------
-# Use afew once we know how it works.
-#
 # Make logging output more palatable.
+#
+# Create a function for logging output.
