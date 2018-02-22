d11b05c41163b36e45de57ca630ce1ef5927a4ed
tminor <thomas.minor@usg.edu>
2018-02-22 11:31:55 -0500

    Comment cleanup

--- .scripts/spam-filter.py
+++ .scripts/spam-filter.py
@@ -1,58 +1,44 @@
-# spam-filter.py
-#
 # This script uses bogofilter to classify spam.
-#
-# TODO
-# -----
-# Need more error handling.
-#
-# Use filepath attribute to log messages classified as spam.
 
 import subprocess
 import notmuch
 import sys
 import os
 
-# Some handy variables.
-
+# create notmuch database stuff
 db = notmuch.Database(mode=1)
+# we only care about new stuff
 query = db.create_query('tag:new')
+# path to binary
 bogofilter = '/usr/local/bin/bogofilter'
 
-# Define a class that we'll use to store a file path
-# and bogofilter spam classification.
-
+# define object that we'll use to store a file path
+# and bogofilter spam classification
 class pOutput(object):
     path = ""
     mailType = ""
 
-# Function for instantiating a bogofilter object.
-
+# function for creating objects out of
+# bogofilter output
 def processOutput(path, mailType):
     processed = pOutput()
     processed.path = path
     processed.mailType = mailType
     return processed
 
+# define classification function
 def isSpam(path):
-    # Run bogofilter.
-    p = subprocess.run(['bogofilter', "-BT", path], stdout=subprocess.PIPE)
-    # Decode its output.
+    p = subprocess.run([bogofilter, "-BT", path], stdout=subprocess.PIPE)
     output = p.stdout.decode('ascii')
-    # Split the output into a list for indexing.
     output = output.split(" ")
-    # Assign index 0 (file path) to the path attribute.
-    # Assign index 1 (H, U, or S classification) to mailType attribute.
     processed = processOutput(output[0], output[1])
-    # If mail is U or H, the message is not spam. Otherwise, it's spam.
+    # output has to be decoded
     if processed.mailType == 'U' or processed.mailType == 'H':
         return False
     if processed.mailType == 'S':
         return True
 
-# Query the notmuch DB with 'tag:new', iterate over all of the matches,
-# and use isSpam to return whether or not the resulting message is spam.
-
+# return all of the filenames that match the tag:new query
 for msg in query.search_messages():
     for filepath in msg.get_filenames():
         if isSpam(filepath) == True:
